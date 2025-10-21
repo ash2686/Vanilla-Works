6602d6d636543d999d4a7d8e57dbb48ff5f36c6b
centaur26 <centaur26@imadechanges.com>
2025-10-22 03:41:47 +1300

    add functionality to get result

--- Lotto/script.js
+++ Lotto/script.js
@@ -1,7 +1,23 @@
 let displayArea = document.querySelector(".ticket-display");
+let drawButton = document.querySelector("#draw");
+let lottoResults = document.querySelector(".lotto-result-numbers");
+let pbResults = document.querySelector(".powerball-result-number");
 let ticket = {};
 let response= true;
+let interval,interval1;
+let counter=0;
+let drawnNumbers = [];
 
+
+let lottoNumber = ()=>{
+    let LN = Math.ceil(Math.random()*40);
+    return LN;
+}
+
+let powerBall = ()=>{
+    let PB = Math.ceil(Math.random()*10);
+    return PB;
+}
 
 function loader(){
     displayArea.innerHTML = '';
@@ -21,20 +37,15 @@ function loader(){
 
 function generateTicket(){
     
-
+    lottoResults.innerHTML = '';
+    pbResults.innerHTML = '';
     displayArea.innerHTML = '';
+    drawnNumbers = [];
+    counter = 0;
     ticket = {};
 
     
-let lottoNumber = ()=>{
-    let LN = Math.ceil(Math.random()*40);
-    return LN;
-}
 
-let powerBall = ()=>{
-    let PB = Math.ceil(Math.random()*10);
-    return PB;
-}
 
 
 function generateLine(){
@@ -79,7 +90,7 @@ for( let key in ticket){
     let eachP;
 
     let powerNumber = powerBall();
-    console.log(`${key} - ${ticket[key]}`);
+    // console.log(`${key} - ${ticket[key]}`);
 
     let lineDiv = document.createElement('div');
     lineDiv.classList.add("ticket-line");
@@ -94,16 +105,21 @@ for( let key in ticket){
 
     for(k=0;k<ticket[key].length;k++){
         eachP = document.createElement('p');
+        eachP.classList.add("number");
         eachP.textContent += ticket[key][k];
         lottoLineDiv.appendChild(eachP);
     }
-        console.log("Each Number Line", lottoLineDiv)
+        // console.log("Each Number Line", lottoLineDiv)
    
 
 
-    let powerBallDiv = document.createElement('p');
+    let powerBallDiv = document.createElement('div');
+    let powerBallNumber = document.createElement('p');
     powerBallDiv.classList.add("power-ball");
-    powerBallDiv.textContent = powerNumber;
+    powerBallNumber.classList.add("pw")
+
+    powerBallNumber.textContent = powerNumber;
+    powerBallDiv.appendChild(powerBallNumber);
 
     lineDiv.append(lineNameDiv,lottoLineDiv,powerBallDiv);
 
@@ -112,6 +128,67 @@ for( let key in ticket){
  
 response = false;
 
+}
+
+function drawResult(){
+    
+    counter++;
+    let newNumber = lottoNumber();
+
+    
+    while(drawnNumbers.includes(newNumber)){
+        newNumber = lottoNumber();
+    }
+
+    drawnNumbers.push(newNumber);
+    
+
+    let newP = document.createElement('p');
+    newP.classList.add("res-disp");
+    newP.textContent = `Number - ${newNumber}`;
+
+    lottoResults.appendChild(newP);
 
    
+
+    
+    let matchedIndex;
+
+   let allNumbers = document.querySelectorAll(".number");
+    allNumbers.forEach((num) => {
+    if (parseInt(num.textContent) === newNumber) {
+      num.classList.add("matched");
+    }
+  });
+
+
+
+    if(counter===6){
+        interval1 = setTimeout(powerBallDraw,1000)
+        clearInterval(interval);
+    }
+}
+
+function powerBallDraw(){
+     let powerNumber = powerBall();
+      let newP = document.createElement("p");
+      newP.classList.add("res-disp");
+      newP.textContent = `Number - ${powerNumber}`;
+
+      pbResults.appendChild(newP);
+
+         let allPowers = document.querySelectorAll(".pw");
+
+      allPowers.forEach((pwr)=>{
+        if(parseInt(pwr.textContent)=== powerNumber){
+            pwr.classList.add("matched");
+        }
+      })
+      clearTimeout(interval1);
+}
+
+drawButton.addEventListener("click",runDraw);
+
+function runDraw(){
+  interval = setInterval(drawResult,1000);
 }
